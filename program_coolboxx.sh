d89ed0d39fd988a69ee0828deecf6de9b27d03db
ESP32andmore <esp32andmore@gmail.com>
2024-06-12 14:57:00 -0400

    Add files via upload

---
+++ program_coolboxx.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+mkdir -p /home/user/coolboxx && cd /home/user/coolboxx
+wget https://github.com/fpovoski/CoolboxxII/raw/main/coolboxx2-factory-V1_0.bin
+wget https://github.com/fpovoski/CoolboxxII/raw/main/coolboxx2-firmware.sh
+chmod +x ./coolboxx2-firmware.sh
+./coolboxx2-firmware.sh ./coolboxx2-factory-V1_0.bin
+cd /home/user
+rm -rf coolboxx
+
